0aa67be57a02f226de3a50acc7f50dc47bdc17fb
이지희 <68178395+Zoe0929@users.noreply.github.com>
2021-06-10 22:36:15 +0900

    프로그래머스 1단계=1

---
+++ Programers/S1-1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+
+{
+
+    return 0;
+
+}
+
+int Knumber(int commands[100][3]) {
+
+
+}
+
+/*범위 기반 for문
+for (int n : arr)
+ n의 값 변화는 적용되지 않음.
+*/
