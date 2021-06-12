2e1c1e847a42fc9cafa197cfd0bd2ca05ad49eac
이지희 <68178395+Zoe0929@users.noreply.github.com>
2021-06-12 23:58:09 +0900

    Vector 이용
    (조건) 벡터 이용해서 문제 풀기

--- Programers/S1-1.cpp
+++ Programers/S1-1.cpp
@@ -1,74 +1,41 @@
-#include <iostream>
+#include <string>
+#include <vector>
+
+
 using namespace std;
 
-int* Knumber(int array[], int commands[][3]);
-void sort(int arr[]);
-void swap(int array[], int index1, int index2);
-
-int main()
-{
-    int arr[] = { 1, 5, 2, 6, 3, 7, 4 };
-    int com[][3] = { {2, 5, 3},{4, 4, 1 }, {1, 7, 3} };
-    int* ret = Knumber(arr, com);
-    for (int t=0 ; t < 4;t++) {
-        cout << ret[t] << endl;
-    }
-
-    return 0;
-
-}
-
-int* Knumber(int array[], int commands[][3]) {
-        int result[50];
-        // 3: 2차원 배열의 세로 크기를 구할 때는 
-        // 배열이 차지하는 전체 공간을 가로 한 줄의 크기로 나눠줌
-
-        for (int g = 0; g < 3; g++) {
-            int temp[100];
-            int i=commands[g][0];
-            int j=commands[g][1];
-            int k=commands[g][2];
-            for (int n = 0; n < j - i + 1; n++) {
-                temp[n] = array[i + n - 1];
-            }
-            sort(temp);
-            result[g]=temp[k-1];
-        }  
-        return result;
-    }
-void sort(int arr[]) {
-        int len = 4;
-        for (int i = 0; i < len - 1; i++) {
-            for (int j = 1; j < len - i; j++) {
-                if (arr[j - 1] > arr[j]) {
-                    swap(arr, j - 1, j);
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    for (int n = 0; n < commands.size(); n++) {
+        vector<int> temp;
+        for (int i=commands[n][0]-1; i<commands[n][1];i++)
+            temp.push_back(array[i]);
+        int t = 0;
+        for (int x = 0; x < temp.size; x++) {
+            for (int y = 0; y < temp.size - x - 1; y++) {
+                if (temp[y] > temp[y + 1]) {
+                    t = temp[y];
+                    temp[y] = temp[y + 1];
+                    temp[y + 1] = t;
                 }
             }
         }
+        answer.push_back(temp[commands[n][2] - 1]);
+
     }
-void swap(int array[], int index1, int index2) {
-        int tempValue = array[index1]; array[index1] = array[index2];
-        array[index2] = tempValue;
-    }
+    return answer;
+}
+
+void main() {
+    vector<int> arr;
+    arr.push_back(1);
+    arr.push_back(5);
+    arr.push_back(2);
+    arr.push_back(6);
+    arr.push_back(3);
+    arr.push_back(7);
+    arr.push_back(4);
 
 
-
-
-
-    /* * 알고리즘 * 
-    1. i = 0; * 
-    2. i가 n-1이면 끝낸다. * 
-    3. j = 1; * 
-    4. j가 n-i가 되면 7로 간다. * 
-    5. a[j-1] > a[j] 이면 두 값을 교환한다, * 
-    6. j를 하나 증가시키고 4로 돌아간다. * 
-    7. i를 하나 증가시키고 2로 돌아간다. */ 
-
-
-
-
-
-/*범위 기반 for문
-for (int n : arr)
- n의 값 변화는 적용되지 않음.
-*/
+    vector<vector<int>> com;
+}
