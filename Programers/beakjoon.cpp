a0e529894229218d7a8eccada6f49719047c09d3
이지희 <68178395+Zoe0929@users.noreply.github.com>
2021-06-15 22:45:22 +0900

    백준 문제 해결
    백준 정답 + STL 사용

--- Programers/beakjoon.cpp
+++ Programers/beakjoon.cpp
@@ -1,68 +1,47 @@
+//STL »ç¿ë
 #include<iostream>
 #include<string>
+#include<stack>
+
 using namespace std;
-const int MAX_STACK_SIZE = 20;
+const int MAX_st_SIZE = 1000;
 
 
 
-class Stack {
-public:
-	int t;
-	int data[];
-	Stack() {
-		t = -1;
-	}
-	void push(int x) {
-		data[++t] = x;
-	}
-	void pop() {
-		if (t > -1) {
-			cout << data[t] << endl;
-			t--;
-		}
-		else {
-			cout << -1 << endl;
-		}
-	}
-	void size() {
-		cout << t + 1 << endl;
-	}
-	void empty() {
-		if (t == -1)
-			cout << 1 << endl;
-		else
-			cout << 0 << endl;
-	}
-	void top() {
-		if (t > -1) {
-			cout << data[t] << endl;
-		}
-		else {
-			cout << -1 << endl;
-		}
-	}
-};
 //10828
 int main() {
 	int num = 0;
 	string str;
-	cin >> num;	
-	Stack stack;
+	cin >> num;
+	stack<int> st;
 	int x;
 	for (int i = 0; i < num; i++) {
 		cin >> str;
 		if (str == "push") {
 			cin >> x;
-			stack.push(x);
+			st.push(x);
 		}
-		else if (str == "pop")
-			stack.pop();
+		else if (str == "pop") {
+			if (!st.empty()) {
+				cout << st.top() << endl;
+				st.pop();
+			}
+			else
+				cout << -1 << endl;
+		}
 		else if (str == "size")
-			stack.size();
-		else if (str == "empty")
-			stack.empty();
+			cout << st.size() << endl;
+		else if (str == "empty") {
+			if (st.empty())
+				cout << 1 << endl;
+			else
+				cout << 0 << endl;
+		}
 		else if (str == "top")
-			stack.top();
+			if (st.empty())
+				cout << -1 << endl;
+			else
+				cout << st.top() << endl;
 	}
 	return 0;
 }
