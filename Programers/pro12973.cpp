255be0bfbe54c9ddcac46d838691f97013e612cb
이지희 <68178395+Zoe0929@users.noreply.github.com>
2021-06-18 17:07:43 +0900

    프로그래머스 짝지어 삭제하기
    성공

--- Programers/pro12973.cpp
+++ Programers/pro12973.cpp
@@ -6,16 +6,18 @@ using namespace std;
 
 int solution(string s)
 {
-    int answer = -1;
-    stack<char> st1;
+    int answer = 0;
+    stack<char> st;
     //무조건 0이 출력되는 경우 :: 입력된 문장이 없거나 글자수가 홀수인 경우에는 무조건 0
     if (s.empty() || s.size() % 2 != 0) {
-        answer = 0;
+        return 0;
     }
 
-    while (0) {
-
-
+    for (int i=0; i<s.size();i++)
+    {
+        if (st.empty() || st.top() != s[i]) st.push(s[i]);
+        else st.pop();
     }
+    if (st.empty()) answer = 1;
     return answer;
 }
