be0f06c9be9631ed0f8e614a12b3099d775f1f20
이지희 <68178395+Zoe0929@users.noreply.github.com>
2021-06-21 02:53:56 +0900

    백준 17298 오큰수
    메모리 초과를 해결하기 위해서 배열 대신 vector를 이용
    하지만 , 여전히 메모리 초과 ..

--- Programers/BJ17298.cpp
+++ Programers/BJ17298.cpp
@@ -2,49 +2,51 @@
 #include<iostream>
 #include<string>
 #include<stack>
+#include<vector>
 
 
 using namespace std;
-
 
 
 int main() {
 	//변수 선언
 
 	int N = 0, idx=0; //배열 A의 크기
-	int A[100001]; //배열 A
+	vector<int> A; //배열 A
 	stack<int> stk;
 
 	//n 입력 받기
 	cin >> N;
 
-	int NEG[100001]; //오큰수를 저장할 배열
+	vector<int> NEG; //오큰수를 저장할 배열
+
+	int a = 0;
+	//배열 A에 저장될 수 입력받기
+	for (int i = 0; i < N; i++) {
+		cin >> a;
+		A.push_back(a);
+	}
 
 	if (N == 1) {
 		NEG[0] = -1;
 		cout << NEG[0] << endl;
 		return 0;
 	}
-
-	//배열 A에 저장될 수 입력받기
-	for (int i = 0; i < N; i++) {
-		cin >> A[i];
-	}
-
 	//배열 A을 다 돌아야되니까 
 	for (int i = 0; i < N; i++) {
 
 		if (stk.empty()) stk.push(A[i]);
+		if (i + 1 == N) NEG.push_back(-1);
 		idx = i;
-		while (1){
+		while (i+1<N){
 			stk.push(A[idx + 1]);
 			if (stk.top() > A[i]) {
-				NEG[i] = stk.top();
+				NEG.push_back(stk.top());
 				while (!stk.empty())  stk.pop(); //stk.clear();
 				break;
 			}
-			else if (idx-1==N || i+1==N) {
-				NEG[i] = -1;
+			else if (idx-1==N ) {
+				NEG.push_back(-1);
 				break;
 			}
 			idx++;
