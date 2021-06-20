2dc271072d45835dc297c7f523d358d8a789f2f7
이지희 <68178395+Zoe0929@users.noreply.github.com>
2021-06-21 03:29:21 +0900

    백준 17298 오큰수
    N이 1인 경우 push_back 함수로 수정 ( 실행 도중 에러 뜨는 현상 해결)

--- Programers/BJ17298.cpp
+++ Programers/BJ17298.cpp
@@ -1,59 +1,59 @@
 
-#include<iostream>
-#include<string>
-#include<stack>
-#include<vector>
+	#include<iostream>
+	#include<string>
+	#include<stack>
+	#include<vector>
 
 
-using namespace std;
+	using namespace std;
 
 
-int main() {
-	//변수 선언
+	int main() {
+		//변수 선언
 
-	int N = 0, idx=0; //배열 A의 크기
-	vector<int> A; //배열 A
-	stack<int> stk;
+		int N = 0, idx=0; //배열 A의 크기
+		vector<int> A; //배열 A
+		stack<int> stk;
 
-	//n 입력 받기
-	cin >> N;
+		//n 입력 받기
+		cin >> N;
 
-	vector<int> NEG; //오큰수를 저장할 배열
+		vector<int> NEG; //오큰수를 저장할 배열
 
-	int a = 0;
-	//배열 A에 저장될 수 입력받기
-	for (int i = 0; i < N; i++) {
-		cin >> a;
-		A.push_back(a);
-	}
+		int a = 0;
+		//배열 A에 저장될 수 입력받기
+		for (int i = 0; i < N; i++) {
+			cin >> a;
+			A.push_back(a);
+		}
 
-	if (N == 1) {
-		NEG[0] = -1;
-		cout << NEG[0] << endl;
+		if (N == 1) {
+			NEG.push_back(-1);
+			cout << NEG[0] << endl;
+			return 0;
+		}
+		//배열 A을 다 돌아야되니까 
+		for (int i = 0; i < N; i++) {
+
+			if (stk.empty()) stk.push(A[i]);
+			if (i + 1 == N) NEG.push_back(-1);
+			idx = i;
+			while (i+1<N){
+				stk.push(A[idx + 1]);
+				if (stk.top() > A[i]) {
+					NEG.push_back(stk.top());
+					while (!stk.empty())  stk.pop(); //stk.clear();
+					break;
+				}
+				else if (idx-1==N ) {
+					NEG.push_back(-1);
+					break;
+				}
+				idx++;
+			}
+			}
+		for (int j=0;j<N;j++)
+			cout <<NEG[j] << " ";
 		return 0;
 	}
-	//배열 A을 다 돌아야되니까 
-	for (int i = 0; i < N; i++) {
-
-		if (stk.empty()) stk.push(A[i]);
-		if (i + 1 == N) NEG.push_back(-1);
-		idx = i;
-		while (i+1<N){
-			stk.push(A[idx + 1]);
-			if (stk.top() > A[i]) {
-				NEG.push_back(stk.top());
-				while (!stk.empty())  stk.pop(); //stk.clear();
-				break;
-			}
-			else if (idx-1==N ) {
-				NEG.push_back(-1);
-				break;
-			}
-			idx++;
-		}
-		}
-	for (int j=0;j<N;j++)
-		cout <<NEG[j] << " ";
-	return 0;
-}
 
