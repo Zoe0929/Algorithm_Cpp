d8642b0a4f950dabb90161ef5feccada8f1dfcdf
이지희 <68178395+Zoe0929@users.noreply.github.com>
2021-06-25 23:42:46 +0900

    프로그래머스 42586 기능개발
    2차 시도

--- Programers/pro42586.cpp
+++ Programers/pro42586.cpp
@@ -6,11 +6,16 @@ using namespace std;
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
     vector<int> days;
+    int i = 0;
     for (int i = 0; i < progresses.size(); i++) {
         days.push_back((100 - progresses[i]) / speeds[i]);
         if (days.size() > 1) {
-            if (days[i - 0] > days[i]) {
-
+            if (days[i - 0] >= days[i]) {
+                i++;
+            }
+            else {
+                answer.push_back(i);
+                i = 0;
             }
         }
     }
