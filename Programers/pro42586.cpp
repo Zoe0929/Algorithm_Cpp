91e2d1add44c44c621ddd869bf955a83bd9c4654
이지희 <68178395+Zoe0929@users.noreply.github.com>
2021-06-26 15:32:53 +0900

    프로그래머스 기능개발
    queue를 이용할 방법 연구 -> 1차 시도, 마지막 요소가 push 안됨.

--- Programers/pro42586.cpp
+++ Programers/pro42586.cpp
@@ -1,23 +1,42 @@
 #include <string>
 #include <vector>
+#include<queue>
+
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
     vector<int> days;
-    int i = 0;
+    queue<int> q;
+
+    int count = 0;
     for (int i = 0; i < progresses.size(); i++) {
-        days.push_back((100 - progresses[i]) / speeds[i]);
-        if (days.size() > 1) {
-            if (days[i - 0] >= days[i]) {
-                i++;
-            }
+        days.push_back(100 - progresses[i] / speeds[i]);
+    }
+    for(int j=0;j<days.size();j++) {
+        if (q.empty()) {
+            q.push(days[j]);
+            count++;
+        }
+        else {
+            if (q.front() > days[j]) {
+                count++;
+                q.push(days[j]);
+        }
             else {
-                answer.push_back(i);
-                i = 0;
+                answer.push_back(count);
+                count = 1;
+                q.push(days[j]);
             }
         }
     }
     return answer;
+}
+
+int main() {
+    vector<int> P = { 93, 30, 55 };
+    vector<int> S = { 1, 30, 5 };
+    solution(P, S);
+    return 0;
 }
