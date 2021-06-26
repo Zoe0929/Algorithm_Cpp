0cd331e19c2837456c3550648637e962cf90dfa3
이지희 <68178395+Zoe0929@users.noreply.github.com>
2021-06-26 17:44:41 +0900

    프로그래머스 프린터
    pair 이용해서 몇번째에 있는지 알아봄 -> 시간초과?

--- Programers/pro42587.cpp
+++ Programers/pro42587.cpp
@@ -6,22 +6,33 @@ using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
-    queue<int> q;
+    queue<pair<int, int>> q;
     int J = priorities[location];
     int count=0;
+
+
     for (int i = 0; i < priorities.size(); i++) {
         if (J <= priorities[i]) {
             count++;
             J = priorities[i];
         }
     }
+
+
     for (int j = 0; j < priorities.size(); j++) {
-        q.push(priorities[j]);
+        q.push(make_pair(priorities[j],j));
     }
     for (int k = 0; k < count; k++) {
         q.push(q.front());
         q.pop();
     }
-    
+    while (!q.empty()) {
+        int C=1;
+        if (q.front().first == location) {
+            answer = C;
+            break;
+        }
+        else C++;
+    }
     return answer;
 }
