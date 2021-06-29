100cc4410e29db581b3d60fa9f16280442e35330
이지희 <68178395+Zoe0929@users.noreply.github.com>
2021-06-29 23:52:41 +0900

    Update BOJ1991.cpp
    입력하는 부분 작성

--- Programers/BOJ1991.cpp
+++ Programers/BOJ1991.cpp
@@ -3,10 +3,19 @@
 #include<vector>
 
 using namespace std;
+
+void inorder() {}
+void postorder() {}
+void preorder() {}
+
 int main() {
 	int N = 0;
+	char Nd1, Nd2;
 	cin >> N;
 	for (int i = 0; i < N; i++) {
-
+		cout << char(18 + i);
+		cin >> Nd1;
+		cin >> Nd2;
 	}
+
 }
