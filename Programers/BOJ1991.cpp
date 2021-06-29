144f979dce93012bba9fe6608017f02a5adb9604
이지희 <68178395+Zoe0929@users.noreply.github.com>
2021-06-30 03:10:01 +0900

    BOJ 1991
    - 노드 구조체 활용
    - 재귀함수 이용
    - 구조체에 왼쪽, 오른쪽 저장
    - ASCII 코드 (char형의 연산)이용

--- Programers/BOJ1991.cpp
+++ Programers/BOJ1991.cpp
@@ -4,18 +4,59 @@
 
 using namespace std;
 
-void inorder() {}
-void postorder() {}
-void preorder() {}
+
+struct Node
+{
+	int Left, Right;
+};
+Node Tree[26];
+
+
+void inorder(int st) {
+	if (Tree[st].Left != -1)inorder(Tree[st].Left);
+	cout << char('A' + st) << ' ';
+	if (Tree[st].Right != -1)inorder(Tree[st].Right);
+}
+
+
+void preorder(int st) {
+	cout << char('A' + st) << ' ';
+	if (Tree[st].Left != -1) preorder(Tree[st].Left);
+	if (Tree[st].Right != -1) preorder(Tree[st].Right);
+}
+
+
+void postorder(int st) {
+	if (Tree[st].Left != -1) postorder(Tree[st].Left);
+	if (Tree[st].Right != -1) postorder(Tree[st].Right);
+	cout << char('A' + st) << ' ';
+}
 
 int main() {
 	int N = 0;
-	char Nd1, Nd2;
 	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cout << char(18 + i);
-		cin >> Nd1;
-		cin >> Nd2;
+
+	for (int i =0 ; i < 26; i++) {
+		Tree[i].Left = Tree[i].Right = -1;
 	}
+
+	for (int i = 0; i < N; i++) {
+		char p, l, r;
+		cin >> p >> l >> r;
+		if (l != '.') {
+			Tree[p - 'A'].Left = l - 'A';
+		}
+		if (r != '.') 
+		{
+			Tree[p - 'A'].Right = r - 'A';
+		}
+	}
+
+	preorder(0);
+	cout << '\n';
+	inorder(0);
+	cout << '\n';
+	postorder(0);
+	cout << '\n';
 
 }
