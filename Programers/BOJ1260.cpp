cbc14ac8d922286ed4f110acb3d3f840cd33d388
이지희 <68178395+Zoe0929@users.noreply.github.com>
2021-07-06 23:30:09 +0900

    백준 1260 DFS,BFS
    1차 시도 -> 원소 하나만 나옴...!!

---
+++ Programers/BOJ1260.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+
+
+
+
+void dfs(int x,vector<int> graph[],bool visited[]) {
+	visited[x] = true;
+	cout << x << ' ';
+	for (int i = 0; i < graph[x].size(); i++) {
+		int y = graph[x][i];
+		if (!visited[y]) dfs(y,graph,visited); //재귀함수, 방문하지 않았다면 방문,DFS 실행
+	}
+}
+
+void bfs(int start,vector<int> graph[],bool visited[]) {
+	queue<int> q;
+	q.push(start);
+	visited[start] = true;
+	while (!q.empty()) {
+		int x = q.front();
+		q.pop();
+		cout << x << endl;
+		for (int i = 0; i < graph[x].size(); i++) {
+			int y = graph[x][i];
+			if (!visited[y]) {
+				q.push(y);
+				visited[y] = true;
+			}
+		}
+	}
+}
+
+
+int main() {
+	int N, M, V;
+	int tmp1,tmp2;
+	cin >> N >> M >> V;
+	bool visited[100];
+	vector<int> graph[100];
+	for (int i = 0; i < M; i++) {
+		cin >> tmp1>>tmp2;
+		graph[tmp1].push_back(tmp2);
+		graph[tmp2].push_back(tmp1);
+	}
+
+	for (int i = 1; i <= N; i++) sort(graph[i].begin(), graph[i].end());
+	dfs(V, graph, visited);
+	cout << endl;
+	bfs(V, graph, visited);
+
+	return 0;
+}
