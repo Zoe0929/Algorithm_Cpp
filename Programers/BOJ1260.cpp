3de696f4eb5fcc2ea2db4da230d195f73380453e
이지희 <68178395+Zoe0929@users.noreply.github.com>
2021-07-07 00:48:06 +0900

    백준 1260 DFS,BFS
    2차 시도 - visited가 초기화 되어있지 않아서 발생하는 에러였음.
    -> Segmentation fault 발생! 허용되지 않은 ㅂ메모리 영역 접근. (처음에 범위를 임의로 100으로 잡아서 발생한 에러였음, 1001으로 고치니 정답 처리됨)

--- Programers/BOJ1260.cpp
+++ Programers/BOJ1260.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 
 void dfs(int x,vector<int> graph[],bool visited[]) {
+
 	visited[x] = true;
 	cout << x << ' ';
 	for (int i = 0; i < graph[x].size(); i++) {
@@ -19,12 +20,14 @@ void dfs(int x,vector<int> graph[],bool visited[]) {
 
 void bfs(int start,vector<int> graph[],bool visited[]) {
 	queue<int> q;
+	
 	q.push(start);
 	visited[start] = true;
+
 	while (!q.empty()) {
 		int x = q.front();
 		q.pop();
-		cout << x << endl;
+		cout << x << ' ';
 		for (int i = 0; i < graph[x].size(); i++) {
 			int y = graph[x][i];
 			if (!visited[y]) {
@@ -40,8 +43,8 @@ int main() {
 	int N, M, V;
 	int tmp1,tmp2;
 	cin >> N >> M >> V;
-	bool visited[100];
-	vector<int> graph[100];
+	bool visited[1001] = { false };
+	vector<int>* graph = new vector<int>[N + 1];
 	for (int i = 0; i < M; i++) {
 		cin >> tmp1>>tmp2;
 		graph[tmp1].push_back(tmp2);
@@ -51,6 +54,7 @@ int main() {
 	for (int i = 1; i <= N; i++) sort(graph[i].begin(), graph[i].end());
 	dfs(V, graph, visited);
 	cout << endl;
+	fill(visited, visited + N + 1, false);
 	bfs(V, graph, visited);
 
 	return 0;
