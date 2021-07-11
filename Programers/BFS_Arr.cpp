4e793edd1927e6972b3d5040b717c5ef32893cf1
이지희 <68178395+Zoe0929@users.noreply.github.com>
2021-07-12 03:40:20 +0900

    백준 2178 미로찾기
    미로찾기 성공 + 배열 이용한 BFS 코드 작성해두기

--- Programers/BFS_Arr.cpp
+++ Programers/BFS_Arr.cpp
@@ -1,18 +1,15 @@
 #include<iostream>
-#include<stack>
 #include<queue>
-#include<string>
 
 using namespace std;
 
 int N, M; //가로 길이가 M이고 세로길이가 N -> map
-int map[100][100] = { 0 };
-int path[100][100];
-bool visited[100][100] = { false };
-int dx[4] = { 1, - 1,0,0 };
+int map[101][101] = { 0 };
+int path[101][101];
+bool visited[101][101] = { false };
+int dx[4] = { 1. - 1.0,0 };
 int dy[4] = { 0,0,-1,1 };
 queue<pair<int, int>> q;
-
 
 void BFS(int y, int x) {
 	path[y][x] = 1;
@@ -32,23 +29,9 @@ void BFS(int y, int x) {
 			if (map[ny][nx] == 1 && visited[ny][nx] == 0) {
 				visited[ny][nx] = true;
 				q.push({ ny,nx });
-				path[ny][nx]=path[y][x]+1; //길을 지나갔음을 표시 
+				path[ny][nx]++; //길을 지나갔음을 표시 
 			}
 
 		}
 	}
-}
-
-
-int main() {
-	cin >> N >> M;
-
-	string str;
-	for (int i = 0; i < N; i++) {
-		cin >> str;
-		for (int j = 0; j < M; j++)
-			map[i][j] = str[j]-'0';
-	}
-	BFS(0, 0);
-	cout << path[N - 1][M - 1];
 }
