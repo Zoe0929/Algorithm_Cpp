6687272bbfda2c0225aee42bcea7cb6a1d02085c
이지희 <68178395+Zoe0929@users.noreply.github.com>
2021-07-19 03:52:16 +0900

    프로그래머스 타켓넘버
    1차 실패

---
+++ Programers/Pro43165.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+int answer = 0;
+
+void DFS(vector<int> numbers, int target, int count=0, int sum=0) {
+    if (count+1==numbers.size()) {
+        if (sum==target)
+        {
+            answer++;
+        }
+        return;
+    }
+    DFS(numbers, target, count + 1, sum - numbers[count + 1]);
+    DFS(numbers, target, count + 1, sum + numbers[count + 1]);
+}
+
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    DFS(numbers, target,0,0);
+    return answer;
+}
