7da75ab07a20a2a0a8f34e2c01939c8c47ddbc1e
Zoe <68178395+Zoe0929@users.noreply.github.com>
2021-12-03 23:39:54 +0900

    Update pro72420.cpp
    살려조

--- Programers/pro72420.cpp
+++ Programers/pro72420.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include<algorithm>
 
 using namespace std;
 
@@ -8,24 +7,21 @@ string solution(string new_id) {
     string answer = "";
     for(int i=0;i<new_id.length();i++){
         if(new_id[i]>=65 && new_id[i]<=90){
-            new_id[i]=tolower(new_id[i]); // 1. 모든 대문자를 소문자로 치환
+            new_id[i]=tolower(new_id[i]);
         }
     }
     for(int i=0;i<new_id.length();i++){
-        if((new_id[i]<='0' && new_id[i]>='9') || new_id[i]!='.' || new_id[i]!='_' || new_id[i]!='-'){
+        if((new_id[i]<=48 && new_id[i]>=57) || new_id[i]!=55 || new_id[i]!=46 || new_id[i]!=95){
             new_id.erase(new_id.begin()+i);
         }
-    }   
-    int i = 1;
-    while (i < new_id.length()){
-        if (new_id[i - 1] == '.' && new_id[i] == ){
-            new_id.erase(new_id.begin() + i);
-            continue;
+    }
+    for(int i=1;i<new_id.length();i++){
+        if(new_id[i]=='.'&&new_id[i-1]=='.'){
+            new_id.erase(new_id.begin()+i);
         }
-        i++;
     }
     if(new_id.front()=='.' ) new_id.erase(new_id.begin());
-    if(new_id.end()=='.') new_id.erase(new_id.length()-1);
+    if(new_id[new_id.length()-1]=='.') new_id.erase(new_id.end()-1);
     if(new_id.length()==0) new_id="a";
     if(new_id.length()>=16){
         new_id.erase(15,new_id.length());
@@ -36,6 +32,5 @@ string solution(string new_id) {
             new_id.push_back(new_id[new_id.length()-1]);
             }while(new_id.length()!=3);
     }
-    answer=new_id;
-    return answer;
+    return new_id;
 }
