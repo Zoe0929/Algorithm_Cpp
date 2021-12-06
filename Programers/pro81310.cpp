b2c99a33f833bcddfb2df5081b05e9d6c3fc724e
Zoe <68178395+Zoe0929@users.noreply.github.com>
2021-12-07 00:36:46 +0900

    Update pro81310.cpp

--- Programers/pro81310.cpp
+++ Programers/pro81310.cpp
@@ -4,60 +4,60 @@
 using namespace std;
 
 int solution(string s) {
-    string answer = 0;
+    string answer = "";
     for(int i=0;i<s.size();i++){
-        if(s[i]=='z'){
-            answer+='0';
-            i+=3;
-        }
-        else if(s[i]=='o'){
-            answer+='1';
-            i+=2;
-        }
-        else if(s[i]=='t' && s[i+1]=='w'){
-            answer+='2';
-            i+=2;
-        }
-        else if(s[i]=='t' && s[i+1]=='h'){
-            answer+='3';
-            i+=4;
-        }
-        else if(s[i]=='f' && s[i+1]=='o'){
-            answer+='4';
-            i+=3;
-        }
+        if(s[i]>='a' && s[i]<='z'){
+            
+            if(s[i]=='z'){
+                answer+='0';
+                i+=3;
+            }
+            else if(s[i]=='o'){
+                answer+='1';
+                i+=2;
+            }
+            else if(s[i]=='t' && s[i+1]=='w'){
+                answer+='2';
+                i+=2;
+            }
+            else if(s[i]=='t' && s[i+1]=='h'){
+                answer+='3';
+                i+=4;
+            }
+            else if(s[i]=='f' && s[i+1]=='o'){
+                answer+='4';
+                i+=3;
+            }
 
-        else if(s[i]=='f' && s[i+1]=='i'){
-            answer+='5';
-            i+=3;
-        }
+            else if(s[i]=='f' && s[i+1]=='i'){
+                answer+='5';
+                i+=3;
+            }
 
-        else if(s[i]=='s' && s[i+1]=='i'){
-            answer+='6';
-            i+=2;
-        }
+            else if(s[i]=='s' && s[i+1]=='i'){
+                answer+='6';
+                i+=2;
+            }
 
-        else if(s[i]=='s' && s[i+1]=='e'){
-            answer+='7';
-            i+=4;
-        }
+            else if(s[i]=='s' && s[i+1]=='e'){
+                answer+='7';
+                i+=4;
+            }
 
-        else if(s[i]=='e'){
-            answer+='8';
-            i+=4;
-        }
+            else if(s[i]=='e'){
+                answer+='8';
+                i+=4;
+            }
 
-        else if(s[i]=='n'){
-            answer+='9';
-            i+=3;
+            else if(s[i]=='n'){
+                answer+='9';
+                i+=3;
+            }
         }
         else {
             answer+=s[i];
         }
     }
-    int num;
-    for(int i=0;i<answer.length();i++){
-        num+=answer[i]*10^(answer.length()-i-1);
-    }
+    int num=stoi(answer);
     return num;
 }
