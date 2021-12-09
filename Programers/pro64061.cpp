61cc9ab7a09f5dc3c51d98810887a41e608f95b6
Zoe <68178395+Zoe0929@users.noreply.github.com>
2021-12-09 20:10:07 +0900

    프로그래머스 크레인인형뽑기

--- Programers/pro64061.cpp
+++ Programers/pro64061.cpp
@@ -1,29 +1,41 @@
+#include <iostream>
 #include <string>
 #include <vector>
-#include<algorithm>
-#include<stdlib.h>
-#include<stack>
+#include <stdlib.h>
+#include <stack>
 
 using namespace std;
 
 int solution(vector<vector<int>> board, vector<int> moves)
 {
     int answer = 0;
-    vector<int> bascket;
+    stack<int> bascket;
 
-    for(int y=0;y<board.size();y++){
-        for(int x=0;x<board[y].size();x++){
-            bascket.push_back(board[y][x]);
-            if(bascket.size()>2){
-                for(int i=0;i<bascket.size();i++){
-                    if(bascket[i]==bascket[i-1]){
-                        answer+=2;
-                        bascket.pop_back();
-                        bascket.pop_back();
-                    }
+    for (int i = 0; i < moves.size(); i++)
+    {
+        int x = moves[i] - 1;
+        for (int y = 0; y < board.size(); y++)
+        {
+            if (board[y][x] != 0)
+            {
+                if (!bascket.empty() && bascket.top() == board[y][x])
+                {
+                    answer += 2;
+                    bascket.pop();
                 }
+                else
+                    bascket.push(board[y][x]);
+                board[y][x] = 0;
+                break;
             }
         }
     }
+
     return answer;
+}
+
+int main(){
+    vector<vector<int>> boards = { { 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 3 }, { 0, 2, 5, 0, 1 }, { 4, 2, 4, 4, 2 }, { 3, 5, 1, 3, 1 }};
+    vector<int> moves = { 1, 5, 3, 5, 1, 2, 1, 4 };
+    cout<<solution(boards,moves)<<endl;
 }
