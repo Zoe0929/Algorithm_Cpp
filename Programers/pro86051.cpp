fc8cbd45a90c797e0f0b97131fde4684e35c3e55
Zoe <68178395+Zoe0929@users.noreply.github.com>
2021-12-11 16:05:03 +0900

    프로그래머스 없는 숫자 더하기

--- Programers/pro86051.cpp
+++ Programers/pro86051.cpp
@@ -1,10 +1,19 @@
 #include <string>
 #include <vector>
+#include<algorithm>
 
 using namespace std;
 
+
+
 int solution(vector<int> numbers)
 {
-    int answer = -1;
+    sort(numbers.begin(),numbers.end());
+    int answer = 0;
+    for (int i=1;i<10;i++){
+        if(*(find(numbers.begin(),numbers.end(),i))!=i){
+            answer+=i;
+        }
+    }
     return answer;
 }
