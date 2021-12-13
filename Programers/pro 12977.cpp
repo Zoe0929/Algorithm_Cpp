1155053f3f00553b5e00bfcbcb7aed43256a4706
Zoe <68178395+Zoe0929@users.noreply.github.com>
2021-12-13 23:31:19 +0900

    Update pro 12977.cpp

--- Programers/pro 12977.cpp
+++ Programers/pro 12977.cpp
@@ -1,14 +1,19 @@
 #include <vector>
 #include <iostream>
-#include<algorithm>
-#include<math.h>
+#include <algorithm>
+#include <math.h>
 using namespace std;
 
-bool IsPrime(int n){
-    if (n==1) return false;
-    if(n==2 || n==3) return true;
-    for(int i=2;i<sqrt(n);i++){
-        if(n%i==0) return false;
+bool IsPrime(int n)
+{
+    if (n == 1)
+        return false;
+    if (n == 2 || n == 3)
+        return true;
+    for (int i = 2; i < sqrt(n); i++)
+    {
+        if (n % i == 0)
+            return false;
     }
     return true;
 }
@@ -17,14 +22,15 @@ int solution(vector<int> nums)
 {
     int answer = 0;
     vector<int> sums;
-    for (int i=0;i<nums.size();i++){
-        for(int j=i+1;j<nums.size();j++){
-            for(int k=j+1;k<nums.size();k++){
-                sums.push_back(i+j+k);
+    for (int i = 0; i < nums.size(); i++)
+    {
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            for (int k = j + 1; k < nums.size(); k++)
+            {
+                if (IsPrime(nums[i] + nums[j] + nums[k]) == true)
+                    answer += 1;
             }
-        }
-        for(int n=0;n<sums.size();n++){
-            if(IsPrime(n)==true) answer++;
         }
     }
     return answer;
