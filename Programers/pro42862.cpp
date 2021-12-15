a25bc965ceed72c4b06b4c1e46f1102c6ec2b003
Zoe <68178395+Zoe0929@users.noreply.github.com>
2021-12-15 22:18:13 +0900

    프로그래머스 체육복

--- Programers/pro42862.cpp
+++ Programers/pro42862.cpp
@@ -1,17 +1,20 @@
 #include <string>
 #include <vector>
-
+#include<algorithm>
 using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
     int answer = 0;
-    int min = n - lost.size();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < reserve.size(); j++) {
-            if (lost[i] == reserve[j] + 1 || lost[i] == reserve[j] - 1) {
-                n++;
-            }
-        }  
+    vector<int> tr_n(n,1);
+    for(int i=0;i<lost.size();i++){
+        tr_n[lost[i]-1]--;
     }
+    for(int j=0;j<reserve.size();j++){
+        tr_n[reserve[j]-1]++;
+    }
+    for(int k=1;k<n;k++){
+        if((tr_n[k-1]==0 && tr_n[k]==2)|| (tr_n[k-1]==2 && tr_n[k]==0)) { tr_n[k]=1; tr_n[k-1]=1;}
+    }
+    answer=count(tr_n.begin(),tr_n.end(),1)+count(tr_n.begin(),tr_n.end(),2);
     return answer;
 }
