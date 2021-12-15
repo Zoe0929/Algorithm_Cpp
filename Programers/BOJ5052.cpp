f5fecd30799207e4a66daf539d783c52824f81cb
Zoe <68178395+Zoe0929@users.noreply.github.com>
2021-12-15 20:14:29 +0900

    백준 전화번호 목록

--- Programers/BOJ5052.cpp
+++ Programers/BOJ5052.cpp
@@ -6,25 +6,28 @@ using namespace std;
 int main() {
     int t=0;
     int n=0;
-    string pn;
-    vector<string> p_num;
     cin>>t;
     for(int i=0;i<t;i++){
         cin>>n;
+        string pn;
+        vector<string> p_num;
         for(int j=0;j<n;j++){
             cin>>pn;
             p_num.push_back(pn);
         }
         sort(p_num.begin(),p_num.end());
-        for(int l=1;l<n;l++){
-            for(int k=0;k<p_num[l].size();k++){
-                if(p_num[l-1][k]==p_num[l][k]) {
-                    printf("NO\n"); 
-                    break;
-                }
-            }
+        // for(int i=0;i<n;i++){
+        //     cout<<p_num[i]<<endl;
+        // }
+        bool answer = true;
+        for (int i = 0; i < p_num.size() - 1; i++)
+        {
+            if ((p_num[i] == p_num[i + 1].substr(0, p_num[i].size())) && (p_num[i].size() != p_num[i+1].size()))
+                answer=false;
         }
-        printf("YES\n");
+
+        if(answer==true) printf("YES\n");
+        else printf("NO\n");
     }
     return 0;
 }
