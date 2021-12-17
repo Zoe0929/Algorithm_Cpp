b1094ddc23aac32e8e6fdaa12d125d8aa280fb2d
Zoe <68178395+Zoe0929@users.noreply.github.com>
2021-12-17 23:28:19 +0900

    프로그래머스 3진법 뒤집기

---
+++ Programers/pro68935.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<int> v;
+    while(1){
+        v.push_back(n%3);
+        n/=3;
+        if(n==1) {
+            v.push_back(1);
+            break;
+        }
+    }
+    for(int i=0;i<v.size();i++){
+        answer+=pow(3,(v.size()-i-1))*v[i];
+    }
+    return answer;
+}
+
+int main() {
+    int n=125;
+    cout<<solution(n);
+}
