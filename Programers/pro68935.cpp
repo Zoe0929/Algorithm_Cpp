1208622d1cb4ef6e241e1433e7586b7c5c6f1c5d
Zoe <68178395+Zoe0929@users.noreply.github.com>
2021-12-18 15:17:09 +0900

    Update pro68935.cpp

--- Programers/pro68935.cpp
+++ Programers/pro68935.cpp
@@ -7,13 +7,10 @@ using namespace std;
 int solution(int n) {
     int answer = 0;
     vector<int> v;
-    while(1){
+    while(n>0){
         v.push_back(n%3);
         n/=3;
-        if(n==1) {
-            v.push_back(1);
-            break;
-        }
+        if(n==1) v.push_back(1);
     }
     for(int i=0;i<v.size();i++){
         answer+=pow(3,(v.size()-i-1))*v[i];
