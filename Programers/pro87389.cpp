94d56f660dda17c16cafd5eb06c9e87566e0b13b
Zoe <68178395+Zoe0929@users.noreply.github.com>
2021-12-21 00:01:14 +0900

    프로그래머스 나머지가 1이 되는 수 찾기

--- Programers/pro87389.cpp
+++ Programers/pro87389.cpp
@@ -6,7 +6,11 @@ using namespace std;
 int solution(int n) {
     int answer = 0;
     for(int i=0;i<n;i++){
-        if(n%i==1) answer=i;
+        if(n%i==1) {
+            answer=i;
+            break;
+        }
+
     }
     return answer;
 }
