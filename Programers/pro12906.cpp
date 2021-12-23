b876fc70caaebe47115c061da0536d00d3bba129
Zoe <68178395+Zoe0929@users.noreply.github.com>
2021-12-24 00:01:11 +0900

    프로그래머스 같은 숫자는 싫어

--- Programers/pro12906.cpp
+++ Programers/pro12906.cpp
@@ -6,11 +6,9 @@ using namespace std;
 vector<int> solution(vector<int> arr) 
 {
     vector<int> answer;
-    unique(arr.begin(),arr.end());
-    return answer;
-}
+    for(int i=1;i<arr.size();i++){
+        if(arr[i-1]!=arr[i]) answer.push_back(arr[i]);
+    }
 
-int main(){
-    vector<int> arr={1,1,3,3,0,1,1};
-    solution(arr);
+    return answer;
 }
