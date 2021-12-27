78ed8ad2a38d04813d21fc17ab362b0cf7a73891
Zoe <68178395+Zoe0929@users.noreply.github.com>
2021-12-27 19:38:19 +0900

    프로그래머스 문자열 내림차순으로 배열하기

---
+++ Programers/pro12917.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+string solution(string s) {
+    sort(s.begin(),s.end(),greater<>());
+    return s;
+}
+
+int main() {
+    solution("Zbcdefg");
+}
