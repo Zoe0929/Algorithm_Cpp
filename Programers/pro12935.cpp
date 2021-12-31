0e4b1dae73f9f0142c122bcb4ecc6a02e430e0a9
Zoe <68178395+Zoe0929@users.noreply.github.com>
2021-12-31 23:58:40 +0900

    Create pro12935.cpp

---
+++ Programers/pro12935.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    if(arr.size()==1){
+        answer.push_back(-1);
+    }
+    else{
+        for(int i=0;i<arr.size();i++){
+            
+        }
+    }
+    return answer;
+}
