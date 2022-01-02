e5a85fd94e1188c8108df87347b4142c0a24a429
Zoe <68178395+Zoe0929@users.noreply.github.com>
2022-01-02 23:49:31 +0900

    프로그래머스 제일 작은 수 제거하기

--- Programers/pro12935.cpp
+++ Programers/pro12935.cpp
@@ -9,8 +9,17 @@ vector<int> solution(vector<int> arr) {
         answer.push_back(-1);
     }
     else{
+        int min=arr[0];
         for(int i=0;i<arr.size();i++){
-            
+            if(min>arr[i]){
+                min=arr[i];
+            }
+        }
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==min) {
+                arr.erase(arr.begin()+i);
+            }
+            answer=arr;
         }
     }
     return answer;
