d83c3b98554eadb7b06a26c1b8e75329df8757b4
Zoe <mam07065@naver.com>
2022-07-07 01:21:31 +0900

    프로그래머스 소수찾기(제곱근 이용)

--- programersStudy/src/boj1978.java
+++ programersStudy/src/boj1978.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class boj1978 {
     public static void main(String[] args) {
-
+        int N=0;
+        int answer=0;
+        Scanner scanner=new Scanner(System.in);
+        N=scanner.nextInt();
+        for(int i=0;i<N;i++){
+            boolean isPrime=true;
+            int num=scanner.nextInt();
+            if(num==1){
+                continue;
+            }
+            for(int j=2;j<=Math.sqrt(num);j++){
+                if(num%j==0){
+                    isPrime=false;
+                    break;
+                }
+            }
+            if(isPrime) answer++;
+        }
+        System.out.println(answer);
     }
 }
 
-class soultion{
-
-}
 
