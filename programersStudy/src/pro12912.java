ffdb124b31818330d83370b61fe6afbc1f76d57b
Zoe <mam07065@naver.com>
2022-07-06 23:44:40 +0900

    프로그래머스 두 정수 사이의 합

---
+++ programersStudy/src/pro12912.java
@@ -0,0 +1,22 @@
+public class pro12912 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Soultion {
+    public long solution(int a, int b) {
+        long answer = 0;
+        if(a<b){
+            for(int i=a;i<=b;i++){
+                answer+=i;
+            }
+        }
+        else{
+            for(int i=b;i<=a;i++){
+                answer+=i;
+            }
+        }
+        return answer;
+    }
+}
