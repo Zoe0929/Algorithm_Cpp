03393481398eadf6c16b4c645b1cb1340df9b676
Zoe <Zoe.urssu@gmail.com>
2023-04-07 00:28:06 +0900

    [SwiftAlgorithm]

--- Algorithm/SwiftAlgorithm/SwiftAlgorithm/main.swift
+++ Algorithm/SwiftAlgorithm/SwiftAlgorithm/main.swift
@@ -7,4 +7,38 @@
 
 import Foundation
 
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
 
+var a = input.reduce(0, +)
+
+print(input)
+print(a)
+
+/*
+func dfs (_ y: Int,_ x: Int) {
+    
+}
+
+let test = Int(readLine()!)!
+
+var check: [[Int]] = Array(repeating: Array(repeating: 0, count: 51), count: 51)
+
+
+for _ in 0..<test{
+    let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+    var arr : [[Int]] = Array(repeating: Array(repeating: 0, count: input[1]), count: input[0])
+    
+    for _ in 0..<input[2]{
+        let loc = readLine()!.split(separator: " ").map{Int(String($0))!}
+        arr[loc[1]][loc[0]] = 1
+        var bug_count = 0
+        for i in 0..<input[1]{
+            for j in 0..<input[0]{
+                if (arr[i][j] != 0) && (check[i][j] == 0) {
+                    
+                }
+            }
+        }
+    }
+}
+*/
